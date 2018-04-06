2a3ed50d0100d166ed8bae32e0a4f3f59af522c6
mandymakweni <mandymakweni1@gmail.com>
2018-04-06 10:26:51 +0200

    pure survey task1

---
+++ pure1/js/table.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    $(".correct").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+  });
+});
+
+
+
+
+
+  $(document).ready(function() {
+    $("#flip").click(function() {
+      $("#panel").slideDown("slow");
+    });
+  });
+
+
+// slideDown
+$(document).ready(function() {
+  $("button").click(function() {
+    $(".correct").fadeIn();
+    $("p").fadeIn("slow");
+    $("p").fadeIn(3000);
+  });
+}); <
